8c1f69f8663513c3dc8f1299f16a45b0fe39c744
savur <h.ozhanli@gmail.com>
2021-01-30 16:53:32 +0300

    project added

---
+++ src/test/java/com/game/imona/GameImonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.imona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameImonaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
